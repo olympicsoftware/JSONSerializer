4a92b48f6b981c08caa409562f1b27beab3e958a
Mike Little <michaell@olympic.co.nz>
2017-06-28 10:54:14 +1200

    Can do subobjects

--- JSONSerializationTests/JSONSerializationTests.swift
+++ JSONSerializationTests/JSONSerializationTests.swift
@@ -26,4 +26,29 @@ class JSONSerializationTests: XCTestCase {
         XCTAssert(json["Species"].string == "Cow")
         XCTAssert(json["Age"].number == 5)
     }
+    
+    
+    struct Person: JSONSerializable {
+        let name: String
+        let age: Int
+        let weight: Double
+        let savings: Decimal
+        let pet: Animal
+    }
+    
+    func testCanSerializeObjectWithNestedObject(){
+        let person = Person(name: "Francis", age: 55, weight: 69.3, savings: 44303.57, pet: Animal(name: "Spike", age: 8, species: "Dog"))
+        let jsonString = person.toJSONString()
+        
+        let json = parseJson(jsonString!)
+        
+        XCTAssert(json["Name"].string == "Francis")
+        XCTAssert(json["Age"].number == 55)
+        XCTAssert(json["Weight"].number == 69.3)
+        XCTAssert(json["Savings"].number == 44303.57)
+        
+        XCTAssert(json["Pet"]["Name"].string == "Spike")
+        XCTAssert(json["Pet"]["Age"].number == 8)
+        XCTAssert(json["Pet"]["Species"].string == "Dog")
+    }
 }
