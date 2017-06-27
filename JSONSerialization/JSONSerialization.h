781b420497115781c024227ec47f5c7fb2099a3f
Mike Little <michaell@olympic.co.nz>
2017-06-27 17:17:43 +1200

    Initial Commit

---
+++ JSONSerialization/JSONSerialization.h
@@ -0,0 +1,19 @@
+//
+//  JSONSerialization.h
+//  JSONSerialization
+//
+//  Created by Mike Little on 27/06/17.
+//  Copyright © 2017 Olympic Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONSerialization.
+FOUNDATION_EXPORT double JSONSerializationVersionNumber;
+
+//! Project version string for JSONSerialization.
+FOUNDATION_EXPORT const unsigned char JSONSerializationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONSerialization/PublicHeader.h>
+
+
